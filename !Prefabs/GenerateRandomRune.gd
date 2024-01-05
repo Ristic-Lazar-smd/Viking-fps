f1caf1389021dbd21b2d2c16966264f9f2977618
aleksam02 <115112258+aleksam02@users.noreply.github.com>
2024-01-05 20:00:57 +0100

    rune swapovanje primitivno

--- !Prefabs/GenerateRandomRune.gd
+++ !Prefabs/GenerateRandomRune.gd
@@ -2,18 +2,30 @@ extends TextureRect
 
 @onready var runeImg1 = preload("res://!Sprites/runeTest.png")
 @onready var runeImg2 = preload("res://!Sprites/runeTest2.png")
+@onready var runeImg3 = preload("res://!Sprites/runeTest3.png")
+@onready var runeImg4 = preload("res://!Sprites/runeTest4.png")
+@onready var runeImg5 = preload("res://!Sprites/runeTest5.png")
+@onready var runeImg6 = preload("res://!Sprites/runeTest6.png")
 
 var rng = RandomNumberGenerator.new()
 var r = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	r = rng.randi_range(1,2)
-	if r!=0:
-		if r==1:
-			set_texture(runeImg1)
-		elif r==2:
-			set_texture(runeImg2)
-
+	#r = rng.randi_range(1,6)
+	#if r!=0:
+	#	if r==1:
+	#		set_texture(runeImg1)
+	#	elif r==2:
+	#		set_texture(runeImg2)
+	#	elif r==3:
+	#		set_texture(runeImg3)
+	#	elif r==4:
+	#		set_texture(runeImg4)
+	#	elif r==5:
+	#		set_texture(runeImg5)
+	#	elif r==2:
+	#		set_texture(runeImg2)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- !Scenes/GameManager.gd
+++ !Scenes/GameManager.gd
@@ -15,26 +15,37 @@ extends Node3D
 @onready var newDexRuneButton = $UI/RunePopUpContainer/NewRunesContainer/NewDexterityRune
 @onready var newMagicRuneButton = $UI/RunePopUpContainer/NewRunesContainer/NewMagicRune
 
+@onready var attackSlotButton = $UI/RunePopUpContainer/RuneSlotsContainer/AttackSlot
+@onready var actionSlotButton = $UI/RunePopUpContainer/RuneSlotsContainer/ActionSlot
+@onready var specialSlotButton = $UI/RunePopUpContainer/RuneSlotsContainer/SpecialSlot
+
 
 
 var isInventoryOpen = false
 var selectedRune
 var selectedRuneTag
-var isAttackSlotOccupied = false
-var isActionSlotOccupied = false
-var isSpecialSlotOccupied = false
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var sRuneInstance = runePrefab.instantiate()
-	strenghtRuneContainer.add_child(sRuneInstance)
+	#var sRuneInstance = runePrefab.instantiate()
+	#strenghtRuneContainer.add_child(sRuneInstance)
 	
-	var dRuneInstance = runePrefab.instantiate()
-	dexRuneContainer.add_child(dRuneInstance)
+	#var dRuneInstance = runePrefab.instantiate()
+	#dexRuneContainer.add_child(dRuneInstance)
 	
-	var mRuneInstance = runePrefab.instantiate()
-	magicRuneContainer.add_child(mRuneInstance)
+	#var mRuneInstance = runePrefab.instantiate()
+	#magicRuneContainer.add_child(mRuneInstance)
+	
+	#var attackRuneInstance = runePrefab.instantiate()
+	#attackRuneSlotContainer.add_child(attackRuneInstance)
+	
+	#var actionRuneInstance = runePrefab.instantiate()
+	#actionRuneSlotContainer.add_child(actionRuneInstance)
+	
+	#var specialRuneInstance = runePrefab.instantiate()
+	#specialRuneSlotContainer.add_child(specialRuneInstance)
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -51,21 +62,41 @@ func _process(delta):
 
 
 func _on_new_strenght_rune_pressed():
-	selectedRune = strenghtRuneContainer.get_child(0)
-	selectedRuneTag = "s"
-	newDexRuneButton.disabled = true
-	newMagicRuneButton.disabled = true
+	if selectedRune!=null:
+		match selectedRuneTag:
+				"at":
+					strenghtRuneContainer.get_child(0).reparent(attackRuneSlotContainer)
+					selectedRune.reparent(strenghtRuneContainer)
+					selectedRune = null
+					newDexRuneButton.disabled = true
+					newMagicRuneButton.disabled = true
+				"sp":
+					strenghtRuneContainer.get_child(0).reparent(specialRuneSlotContainer)
+					selectedRune.reparent(strenghtRuneContainer)
+					selectedRune = null
+					newDexRuneButton.disabled = true
+					newMagicRuneButton.disabled = true
+				"ac":
+					strenghtRuneContainer.get_child(0).reparent(actionRuneSlotContainer)
+					selectedRune.reparent(strenghtRuneContainer)
+					selectedRune = null
+					newDexRuneButton.disabled = true
+					newMagicRuneButton.disabled = true
+	else:
+		selectedRune = strenghtRuneContainer.get_child(0)
+		selectedRuneTag = "s"
+		newStrenghtRuneButton.grab_focus()
+		newDexRuneButton.disabled = true
+		newMagicRuneButton.disabled = true
 
 
 func _on_attack_slot_pressed():
 	if selectedRune == null and attackRuneSlotContainer.get_child(0)!=null:
 		selectedRune = attackRuneSlotContainer.get_child(0)
+		selectedRuneTag = "at"
+		attackSlotButton.grab_focus()
 	else:
-		if selectedRune != null and isAttackSlotOccupied==false:
-			selectedRune.reparent(attackRuneSlotContainer)
-			selectedRune = null
-			isAttackSlotOccupied = true
-		elif selectedRune != null and isAttackSlotOccupied==true:
+		if selectedRune != null:
 			match selectedRuneTag:
 				"s":
 					attackRuneSlotContainer.get_child(0).reparent(strenghtRuneContainer)
@@ -79,25 +110,130 @@ func _on_attack_slot_pressed():
 					attackRuneSlotContainer.get_child(0).reparent(magicRuneContainer)
 					selectedRune.reparent(attackRuneSlotContainer)
 					selectedRune = null
+				"ac":
+					attackRuneSlotContainer.get_child(0).reparent(actionRuneSlotContainer)
+					selectedRune.reparent(attackRuneSlotContainer)
+					selectedRune = null
+				"sp":
+					attackRuneSlotContainer.get_child(0).reparent(specialRuneSlotContainer)
+					selectedRune.reparent(attackRuneSlotContainer)
+					selectedRune = null
 
 
 func _on_new_dexterity_rune_pressed():
-	selectedRune = dexRuneContainer.get_child(0)
+	if selectedRune!=null:
+		match selectedRuneTag:
+				"at":
+					dexRuneContainer.get_child(0).reparent(attackRuneSlotContainer)
+					selectedRune.reparent(dexRuneContainer)
+					selectedRune = null
+					newStrenghtRuneButton.disabled = true
+					newMagicRuneButton.disabled = true
+				"sp":
+					dexRuneContainer.get_child(0).reparent(specialRuneSlotContainer)
+					selectedRune.reparent(dexRuneContainer)
+					selectedRune = null
+					newStrenghtRuneButton.disabled = true
+					newMagicRuneButton.disabled = true
+				"ac":
+					dexRuneContainer.get_child(0).reparent(actionRuneSlotContainer)
+					selectedRune.reparent(dexRuneContainer)
+					selectedRune = null
+					newStrenghtRuneButton.disabled = true
+					newMagicRuneButton.disabled = true
+	else:
+		selectedRune = dexRuneContainer.get_child(0)
+		selectedRuneTag = "d"
+		newDexRuneButton.grab_focus()
+		newStrenghtRuneButton.disabled = true
+		newMagicRuneButton.disabled = true
 
 
 func _on_new_magic_rune_pressed():
-	selectedRune = magicRuneContainer.get_child(0)
+	if selectedRune!=null:
+		match selectedRuneTag:
+				"at":
+					magicRuneContainer.get_child(0).reparent(attackRuneSlotContainer)
+					selectedRune.reparent(magicRuneContainer)
+					selectedRune = null
+					newDexRuneButton.disabled = true
+					newStrenghtRuneButton.disabled = true
+				"sp":
+					magicRuneContainer.get_child(0).reparent(specialRuneSlotContainer)
+					selectedRune.reparent(magicRuneContainer)
+					selectedRune = null
+					newDexRuneButton.disabled = true
+					newStrenghtRuneButton.disabled = true
+				"ac":
+					magicRuneContainer.get_child(0).reparent(actionRuneSlotContainer)
+					selectedRune.reparent(magicRuneContainer)
+					selectedRune = null
+					newDexRuneButton.disabled = true
+					newStrenghtRuneButton.disabled = true
+					
+	else:
+		selectedRune = magicRuneContainer.get_child(0)
+		selectedRuneTag = "m"
+		newMagicRuneButton.grab_focus()
+		newDexRuneButton.disabled = true
+		newStrenghtRuneButton.disabled = true
 
 
 func _on_action_slot_pressed():
-	if selectedRune != null and isActionSlotOccupied==false:
-		selectedRune.reparent(actionRuneSlotContainer)
-		selectedRune = null
-		isActionSlotOccupied = true
+	if selectedRune == null and actionRuneSlotContainer.get_child(0)!=null:
+		selectedRune = actionRuneSlotContainer.get_child(0)
+		selectedRuneTag = "ac"
+		actionSlotButton.grab_focus()
+	else:
+		if selectedRune != null:
+			match selectedRuneTag:
+				"s":
+					actionRuneSlotContainer.get_child(0).reparent(strenghtRuneContainer)
+					selectedRune.reparent(actionRuneSlotContainer)
+					selectedRune = null
+				"d":
+					actionRuneSlotContainer.get_child(0).reparent(dexRuneContainer)
+					selectedRune.reparent(actionRuneSlotContainer)
+					selectedRune = null
+				"m":
+					actionRuneSlotContainer.get_child(0).reparent(magicRuneContainer)
+					selectedRune.reparent(actionRuneSlotContainer)
+					selectedRune = null
+				"at":
+					actionRuneSlotContainer.get_child(0).reparent(attackRuneSlotContainer)
+					selectedRune.reparent(actionRuneSlotContainer)
+					selectedRune = null
+				"sp":
+					actionRuneSlotContainer.get_child(0).reparent(specialRuneSlotContainer)
+					selectedRune.reparent(actionRuneSlotContainer)
+					selectedRune = null
 
 
 func _on_special_slot_pressed():
-	if selectedRune != null and isSpecialSlotOccupied==false:
-		selectedRune.reparent(specialRuneSlotContainer)
-		selectedRune = null
-		isSpecialSlotOccupied = true
+	if selectedRune == null and specialRuneSlotContainer.get_child(0)!=null:
+		selectedRune = specialRuneSlotContainer.get_child(0)
+		selectedRuneTag = "sp"
+		specialSlotButton.grab_focus()
+	else:
+		if selectedRune != null:
+			match selectedRuneTag:
+				"s":
+					specialRuneSlotContainer.get_child(0).reparent(strenghtRuneContainer)
+					selectedRune.reparent(specialRuneSlotContainer)
+					selectedRune = null
+				"d":
+					specialRuneSlotContainer.get_child(0).reparent(dexRuneContainer)
+					selectedRune.reparent(specialRuneSlotContainer)
+					selectedRune = null
+				"m":
+					specialRuneSlotContainer.get_child(0).reparent(magicRuneContainer)
+					selectedRune.reparent(specialRuneSlotContainer)
+					selectedRune = null
+				"at":
+					specialRuneSlotContainer.get_child(0).reparent(attackRuneSlotContainer)
+					selectedRune.reparent(specialRuneSlotContainer)
+					selectedRune = null
+				"ac":
+					specialRuneSlotContainer.get_child(0).reparent(actionRuneSlotContainer)
+					selectedRune.reparent(specialRuneSlotContainer)
+					selectedRune = null
